571e0266994589bff8ef85d9acc446c236f6538e
ingong <insong1216@gmail.com>
2022-03-30 19:37:59 +0900

    [FIX]: 사이드바 컴포넌트 타입 체킹관련 수정

--- components/Modals/index.tsx
+++ components/Modals/index.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import SideBar from '@components/SideBar';
 import { CountryDropdown, AirportDropdown } from '@components/Dropdown';
 import { ModalsStateContext } from '@context/ModalsContext';
-import { ModalListStateType } from './moda.type';
+import { ModalListStateType } from './modal';
 
 export const modalList = {
   SideBar,
@@ -17,11 +17,12 @@ const Modals = () => {
     <>
       {openedModals?.map((modal, index) => {
         const { Component, props } = modal;
-        const { onSubmit, onClose, restProps } = props;
+        if (!props || !props.onSubmit || !props.onClose) return <Component key={index} />;
 
-        const handleSubmit = async (value?: string) => {
+        const { onSubmit, onClose, restProps } = props;
+        const handleSubmit = async (value: string) => {
           if (typeof onSubmit === 'function') await onSubmit(value);
-          onClose();
+          onClose(Component);
         };
 
         return (
