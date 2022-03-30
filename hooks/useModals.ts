21a02213ca500adeedb17603bbb165db9646c28f
ingong <insong1216@gmail.com>
2022-03-30 19:37:12 +0900

    [REFACTOR]: Component 타입 관련 수정

--- hooks/useModals.ts
+++ hooks/useModals.ts
@@ -1,18 +1,18 @@
-import { useContext } from 'react';
+import { useContext, ReactPortal } from 'react';
 import { ModalsDispatchContext } from '@context/ModalsContext';
-import { ModalsDispatch } from '@Customtypes/modal';
+import { ModalPropType } from '@components/Modals/modal';
 
 export default function useModals() {
-  const { open, close } = useContext(ModalsDispatchContext) as ModalsDispatch;
+  const { open, close } = useContext(ModalsDispatchContext);
   const preventScroll = () => (document.body.style.overflow = 'hidden');
   const ableScroll = () => (document.body.style.overflow = 'scroll');
 
-  const openModal = (Component: React.ReactNode, props?: object) => {
+  const openModal = (Component: () => ReactPortal | JSX.Element, props?: ModalPropType) => {
     open(Component, props);
     preventScroll();
   };
 
-  const closeModal = (Component: React.ReactNode) => {
+  const closeModal = (Component: () => ReactPortal | JSX.Element) => {
     close(Component);
     ableScroll();
   };
